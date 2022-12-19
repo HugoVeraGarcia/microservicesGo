288c24c6bd63bd7f02c5e989789b2af4c13e8113
HugoVeraGarcia <hugo.vera.garcia@gmail.com>
2022-12-19 13:53:22 -0500

    First Commit

---
+++ authentication-service/cmd/api/routes.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+// en el folder authentication service
+// go get github.com/go-chi/chi/v5
+// go get github.com/go-chi/chi/v5/middleware
+// go get github.com/go-chi/chi/cors
+func (app *Config) routes() http.Handler{
+
+}
